e6aa22fd6dbc3484b75c16c5413551ddaaf14f4e
e16din <bigusov@gmail.com>
2016-08-04 17:45:18 +0300

    add Intent.ACTION_VIEW methods to Start

--- src/main/java/com/e16din/intentmaster/Start.java
+++ src/main/java/com/e16din/intentmaster/Start.java
@@ -3,8 +3,10 @@ package com.e16din.intentmaster;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
 import android.support.v4.app.Fragment;
 
 import com.e16din.intentmaster.model.Data;
@@ -17,22 +19,64 @@ public final class Start {
         super();
     }
 
-    public static void activity(Context context, @NonNull Class cls) {
+    /**
+     * Start an activity with
+     * intent.setAction(Intent.ACTION_VIEW);
+     * intent.setData(dataUri)
+     *
+     * @param context context
+     * @param dataUri data URI string
+     */
+    public static void activity(@NonNull Context context, @NonNull String dataUri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(dataUri));
+        context.startActivity(intent);
+    }
+
+    /**
+     * Start an activity with
+     * intent.setAction(Intent.ACTION_VIEW);
+     * intent.setData(dataUri)
+     *
+     * @param context context
+     * @param dataUri data URI string resId
+     */
+    public static void activity(@NonNull Context context, @StringRes int dataUri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(context.getString(dataUri)));
+        context.startActivity(intent);
+    }
+
+    /**
+     * Start an activity with
+     * intent.setAction(Intent.ACTION_VIEW);
+     * intent.setData(dataUri)
+     *
+     * @param context context
+     * @param dataUri data URI
+     */
+    public static void activity(@NonNull Context context, @NonNull Uri dataUri) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(dataUri);
+        context.startActivity(intent);
+    }
+
+    public static void activity(@NonNull Context context, @NonNull Class cls) {
         Intent intent = Create.intent(context, cls);
         context.startActivity(intent);
     }
 
-    public static void activity(Context context, @NonNull Class cls, Data... data) {
+    public static void activity(@NonNull Context context, @NonNull Class cls, Data... data) {
         Intent intent = Create.intent(context, cls, data);
         context.startActivity(intent);
     }
 
-    public static void activity(Context context, @NonNull Class cls, Serializable... data) {
+    public static void activity(@NonNull Context context, @NonNull Class cls, Serializable... data) {
         Intent intent = Create.intent(context, cls, data);
         context.startActivity(intent);
     }
 
-    public static void activity(Context context, @NonNull Class cls, Parcelable... data) {
+    public static void activity(@NonNull Context context, @NonNull Class cls, Parcelable... data) {
         Intent intent = Create.intent(context, cls, data);
         context.startActivity(intent);
     }
